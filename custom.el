3e835aaf4d28f959b7031a179d5edeffb29e8100
aarnej <aarnej@iki.fi>
2019-03-28 15:27:01 +0200

    flycheck, custom magit, etc.

--- custom.el
+++ custom.el
@@ -4,54 +4,81 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(Buffer-menu-name-width 60)
- '(ag-arguments (quote ("--smart-case" "--statss")))
+ '(ag-arguments (quote ("--smart-case" "--stats" "--width=100")))
+ '(ag-highlight-search t)
+ '(auto-save-default nil)
  '(column-number-mode t)
  '(confirm-nonexistent-file-or-buffer t)
+ '(css-indent-offset 2)
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults)))
  '(elpy-rpc-backend "jedi")
  '(elpy-shell-echo-input-cont-prompt nil)
  '(fill-column 79)
+ '(flycheck-shellcheck-follow-sources nil)
  '(git-commit-fill-column 70)
  '(global-whitespace-mode t)
+ '(ibuffer-elide-long-columns nil)
+ '(ibuffer-formats
+   (quote
+    ((mark modified read-only locked " "
+           (name 60 60 :left :elide)
+           " " filename-and-process " ")
+     (mark " "
+           (name 16 -1)
+           " " filename))))
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(kill-ring-max 1000)
  '(magit-diff-refine-hunk (quote all))
+ '(magit-fetch-arguments (quote ("--tags")))
  '(magit-gerrit-push-review-to-topic nil)
  '(magit-log-arguments (quote ("-n256" "--graph" "--decorate")))
  '(magit-log-margin-spec (quote (28 1 magit-duration-spec)))
  '(magit-log-section-arguments (quote ("-n256")))
+ '(magit-log-section-commit-count 20)
  '(magit-log-show-margin nil)
  '(magit-reflog-show-margin nil)
  '(magit-refs-sections-hook
    (quote
     (magit-insert-error-header magit-insert-branch-description magit-insert-local-branches magit-insert-remote-branches)))
- '(magit-status-sections-hook
-   (quote
-    (magit-insert-status-headers magit-insert-merge-log magit-insert-rebase-sequence magit-insert-am-sequence magit-insert-sequencer-sequence magit-insert-bisect-output magit-insert-bisect-rest magit-insert-bisect-log magit-insert-untracked-files magit-insert-unstaged-changes magit-insert-staged-changes magit-insert-stashes magit-insert-unpushed-to-pushremote magit-insert-unpushed-to-upstream-or-recent magit-insert-unpulled-from-pushremote magit-insert-unpulled-from-upstream magit-insert-ignored-files)))
  '(make-backup-files nil)
  '(mode-line-format
    (quote
-    ("" mode-line-client mode-line-modified mode-line-frame-identification mode-line-buffer-identification " " mode-line-misc-info mode-line-position
-     (vc-mode vc-mode)
-     "  " mode-line-modes mode-line-remote mode-line-end-spaces)))
+    ("" mode-line-client mode-line-modified mode-line-frame-identification
+     (:eval
+      (format "[%s]"
+              (projectile-project-name)))
+     " " mode-line-buffer-identification mode-line-misc-info mode-line-position mode-line-remote mode-line-end-spaces)))
  '(mode-require-final-newline nil)
  '(package-selected-packages
    (quote
-    (xcscope use-package undo-tree projectile popup magit-svn js2-mode ido-vertical-mode helm-core flx-ido fiplr browse-kill-ring ag adoc-mode)))
+    (transient request rjsx-mode flycheck dired-filter python-docstring json-mode yaml-mode xterm-color xcscope use-package undo-tree projectile popup js2-mode ido-vertical-mode helm-core flx-ido fiplr browse-kill-ring ag adoc-mode)))
+ '(projectile-mode-line
+   (quote
+    (:eval
+     (format " Pj[%s]"
+             (projectile-project-name)))))
+ '(python-docstring-sentence-end-double-space nil)
  '(python-fill-docstring-style (quote django))
  '(python-shell-interpreter "python3")
  '(recentf-max-saved-items 1000)
  '(recentf-mode t)
+ '(request-curl-options (quote ("--netrc")))
  '(require-final-newline nil)
  '(show-paren-mode t)
  '(show-paren-style (quote mixed))
  '(tab-width 4)
+ '(tar-mode-show-date t)
  '(truncate-partial-width-windows nil)
+ '(undo-tree-mode-lighter " Undo-T")
  '(vc-display-status nil)
  '(which-function-mode t)
  '(whitespace-global-modes (quote (python-mode)))
  '(whitespace-line-column 79)
- '(whitespace-style (quote (face trailing lines-tail tab-mark))))
+ '(whitespace-style (quote (face trailing lines-tail tab-mark)))
+ '(yaml-block-literal-electric-alist (quote ((124 . "") (62 . "")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -7,22 +7,89 @@
 
 (use-package adoc-mode
   :ensure t
-  :mode (("\\.adoc\\'" . adoc-mode)))
+  :bind (:map adoc-mode-map ("C-c C-t" . adoc-adjust-title-del))
+  :mode (("\\.adoc\\'" . adoc-mode))
+  :config
+  (add-hook 'adoc-mode-hook
+          (lambda ()
+            (electric-indent-local-mode 0))))
+
+(use-package xterm-color
+  :ensure t
+  :config
+  (setq comint-output-filter-functions
+        (remove 'ansi-color-process-output comint-output-filter-functions))
+  (add-hook 'shell-mode-hook
+            (lambda ()
+              (add-hook
+               'comint-preoutput-filter-functions
+               'xterm-color-filter nil t))))
 
 (use-package ag
   :ensure t
   :bind ("\C-cg" .
 	 (lambda () (interactive)
 	   (setq current-prefix-arg '(4)) ; C-u
-	   (call-interactively 'ag))))
+	   (call-interactively 'ag)))
+  :config
+  (add-hook 'ag-mode-hook (lambda () (setq truncate-lines t))))
+
+(use-package dired-filter
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :config
+  (setq-default flycheck-disabled-checkers
+                (append flycheck-disabled-checkers
+                        '(javascript-jshint)))
+  (global-flycheck-mode)
+
+  (defun my/use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (eslint (and root
+                        (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                          root))))
+      (when (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint))))
+
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+  (defvaralias 'flycheck-python-flake8-executable 'python-shell-interpreter)
+  )
+
+
+(use-package rjsx-mode
+  :ensure t
+  :mode "\\.js\\'")
+
+(use-package js2-mode
+  :ensure t
+  ;;:mode "\\.js\\'"
+)
 
 (use-package browse-kill-ring
   :ensure t
   :config
   (browse-kill-ring-default-keybindings))
 
-(use-package elpy
+(use-package yaml-mode
   :ensure t)
+
+(use-package python-docstring
+  :ensure t
+  :config
+  (python-docstring-install))
+
+(use-package json-mode
+  :ensure t)
+
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable))
 
 (use-package flx-ido
   :ensure t
@@ -39,9 +106,58 @@
   :config
   (ido-vertical-mode))
 
-(use-package magit
+(use-package request
   :ensure t
-  :bind (("C-x v l" . magit-log-buffer-file)))
+  )
+
+(use-package magit
+  :load-path "lisp/magit/lisp"
+  :commands (magit-status)
+  :bind (("C-x v l" . magit-log-buffer-file)
+         ("C-x v =" . magit-diff-buffer-file)
+         ("C-x g" . magit-status))
+  :config
+  (magit-define-popup-switch 'magit-fetch-popup
+      ?t "Fetch all tags" "--tags")
+
+  (defun magit-insert-aarne-gerrit-reviews ()
+    (let* ((commits (--filter (string-match "Change-Id" it)
+                              (magit-git-lines "log" "@{upstream}..")))
+           (change-ids (--map (substring it 15) commits))
+           (query (string-join
+                   (--map (concat "change:" it) change-ids) " OR "))
+           (json ["1" "2" "3"])
+           (req (request
+                 "https://gerrit.ericsson.se/a/changes/"
+                 :params (list (cons "q" query))
+                 :parser (lambda ()
+                           (forward-line) ;; skip XSSI prevention magic
+                           (json-read))
+                 :sync t))
+           (data (request-response-data req))
+           )
+
+      (magit-insert-section (reviews nil t)
+        (magit-insert-heading "Reviews")
+        (mapc (lambda (it)
+                (magit-insert-section (review)
+                  (insert (cdr (assoc 'subject it)) ?\n)))
+              data)
+        (insert ?\n)
+        )
+      )
+    )
+
+  (magit-add-section-hook 'magit-status-sections-hook
+			              'magit-insert-aarne-gerrit-reviews
+                          'magit-insert-unpulled-from-pushremote)
+  (magit-add-section-hook 'magit-status-sections-hook
+			              'magit-insert-ignored-files
+                          nil t)
+  (magit-add-section-hook 'magit-status-sections-hook
+			              'magit-insert-worktrees
+                          nil t)
+  )
 
 (use-package magit-gerrit
   :load-path "lisp/magit-gerrit"
@@ -61,8 +177,7 @@
 
 (use-package robot-mode
   :load-path "lisp/robot-mode"
-  :config
-  (add-to-list 'auto-mode-alist '("\\.robot\\'" . robot-mode)))
+  :mode "\\.robot\\'")
 
 (use-package undo-tree
   :ensure t
@@ -80,6 +195,8 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
 
 ;; to get CTRL + arrow keys working
 ;;(define-key input-decode-map "\e[1;3A" [C-up])
@@ -103,7 +220,6 @@
 	  (lambda ()
 	    (delete 'indentation::space (symbol-value 'whitespace-style))))
 
-
 (load-file "~/.emacs.d/defun.el")
 (global-set-key (kbd "C-c s")  'yank-isearch-string)
 (add-hook 'find-file-hook 'commit_msg_hook)
@@ -111,3 +227,4 @@
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+(put 'magit-clean 'disabled nil)
